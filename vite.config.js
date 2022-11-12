cbfc6b23cbaeefffd2306a1634c3955f69206565
Víctor Ruiz <victor.ruiz.mor@gmail.com>
2022-11-11 21:34:55 -0600

    :rocket: vite
    Configuration vite

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		lib: {
-			entry: 'index.js',
+			entry: '/index.js',
 			formats: ['es'],
 		},
 		rollupOptions: {
 			external: /^lit/,
 		},
-	},
-	server: {
-		port: 3000,
-    open: true
 	}
 });
